c54ec466f2debc6281290ad2a2d6d716fdf7ff9d
Shivam Mandloi <shivammandloi1102@gmail.com>
2024-04-21 12:05:55 +0530

    add PrintFunction

--- include/Function.hpp
+++ include/Function.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <iostream>
 #include "Node.hpp"
 
 class Function
 {
 public:
-    struct Node* CreateNode(NodeType nodeType, std::string value, struct Node *leftNode = nullptr, struct Node *rightNode = nullptr)
+    struct Node *CreateNode(NodeType nodeType, std::string value, struct Node *leftNode = nullptr, struct Node *rightNode = nullptr)
     {
         /*
             Page No. 6
@@ -17,5 +18,37 @@ public:
         node->leftNode = leftNode;
         node->rightNode = rightNode;
         return node;
+    }
+
+    void PrintFunction(struct Node *node)
+    {
+        /*
+            Page No. 8
+            Heading: Print Function
+        */
+        if (node == nullptr)
+            return;
+        else if (node->type == OPERATOR)
+        {
+            std::cout << "(";
+            PrintFunction(node->leftNode);
+            std::cout << node->value;
+            PrintFunction(node->rightNode);
+            std::cout << ")";
+        }
+        else if (node->type == FUNCTION)
+        {
+            std::cout << node->value << "(";
+            PrintFunction(node->leftNode);
+            std::cout << ")";
+        }
+        else if (node->type == VARIABLE)
+        {
+            std::cout << node->value;
+        }
+        else if (node->type == CONSTANT)
+        {
+            std::cout << node->value;
+        }
     }
 };
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 int main()
 {
     Function fn = Function();
-    struct Node* node = fn.CreateNode(OPERATOR, "+", fn.CreateNode(VARIABLE, "x"), fn.CreateNode(CONSTANT, "2"));
+    struct Node* node = fn.CreateNode(OPERATOR, "+", fn.CreateNode(VARIABLE, "x"), fn.CreateNode(FUNCTION, "sin", fn.CreateNode(OPERATOR , "+", fn.CreateNode(OPERATOR, "^", fn.CreateNode(VARIABLE, "x"), fn.CreateNode(CONSTANT, "3")), fn.CreateNode(CONSTANT, "4"))));
+    fn.PrintFunction(node);
+    std::cout << std::endl;
     return 0;
 }
